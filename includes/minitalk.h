6d7ac7156981acd8ec430df5ced0ba8ffd633618
abonte-l <66364998+abonte-l@users.noreply.github.com>
2022-06-17 15:39:26 +0200

    Add files via upload

---
+++ includes/minitalk.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abonte-l <abonte-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 15:02:37 by abonte-l          #+#    #+#             */
+/*   Updated: 2022/01/02 18:15:44 by abonte-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <sys/types.h>
+
+# define ERROR -1
+# define SUCCESS 0
+
+# define ERROR_ARG_SERV "[ERROR]\n./server needs ONLY 1 argument : exit\n"
+# define ERROR_ARG_CLI "[ERROR]\n./client works with 3 arguments ONLY : exit\n"
+# define ERROR_PID "[ERROR]\nincorrect server's PID (0 or lower) : exit\n"
+# define ERROR_SIGNAL "[ERROR]\nsignal error : exit\n"
+# define ERROR_SIGAC_SERV "[ERROR]\nError server sigaction\n"
+# define ERROR_SIGAC_CLI "[ERROR]\nError client sigaction\n"
+# define PID_SUCCESS "[SERVER LISTENING] PID successfully retrieve : "
+
+int		ft_atoi(const char *str);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nbr);
+void	ft_putchar(char c);
+
+#endif
---
+++ sources/client.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abonte-l <abonte-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 14:57:33 by abonte-l          #+#    #+#             */
+/*   Updated: 2022/01/01 17:42:18 by abonte-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minitalk.h"
+
+void	sending_bits(char c, int pid)
+{
+	int	i;
+
+	i = 128;
+	while (i >= 1)
+	{
+		if (i & c)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				ft_putstr(ERROR_SIGNAL);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				ft_putstr(ERROR_SIGNAL);
+				exit(EXIT_FAILURE);
+			}
+		}
+		i /= 2;
+		usleep(1000);
+	}
+}
+
+int	send_str(int pid, char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		sending_bits(s[i], pid);
+		i++;
+	}
+	return (0);
+}
+
+void	talk_handler(int signum, siginfo_t *siginfo, void *context)
+{
+	(void)context;
+	(void)siginfo;
+	(void)signum;
+	return ;
+}
+
+int	checker_arg_pid(char *str, int ac)
+{
+	int	pid;
+
+	pid = 0;
+	if (ac != 3)
+	{
+		ft_putstr(ERROR_ARG_CLI);
+		exit(EXIT_FAILURE);
+	}
+	pid = ft_atoi(str);
+	if (!(pid <= 0))
+		return (pid);
+	ft_putstr(ERROR_PID);
+	exit(EXIT_FAILURE);
+}
+
+int	main(int ac, char **av)
+{
+	struct sigaction	sa;
+	int					pid;
+
+	pid = checker_arg_pid(av[1], ac);
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGINT);
+	sigaddset(&sa.sa_mask, SIGQUIT);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = talk_handler;
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_putstr(ERROR_SIGAC_CLI);
+	send_str(pid, av[2]);
+	return (SUCCESS);
+}
